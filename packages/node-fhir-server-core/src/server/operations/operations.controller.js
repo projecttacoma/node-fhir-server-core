eb00aea31b8d060e0e03313999cf2c304d867b6e
sarahmcdougall <smcdougall@mitre.org>
2021-10-12 13:35:10 -0400

    removed unneeded dependency

--- packages/node-fhir-server-core/src/server/operations/operations.controller.js
+++ packages/node-fhir-server-core/src/server/operations/operations.controller.js
@@ -1,34 +1,5 @@
 const handler = require('@asymmetrik/fhir-response-util');
 
-const express = require('express');
-const app = express();
-/**
- * Override of Express's status() function to check if a status exists before
- * setting the status to the specified statusCode
- * @param {number} statusCode
- */
-// express.response.status = function (statusCode) {
-//   if (this.response) {
-//     if (this.statusCode === undefined) {
-//       this.statusCode = statusCode;
-//     }
-//   }
-// };
-
-function setResponseStatus(res, statusCode) {
-  if (res.statusCode === undefined) {
-    res.statusCode = statusCode;
-  }
-}
-
-// brought directly in from fhir-response-util for testing
-
-function read(req, res, json) {
-  let fhirVersion = req.params.base_version;
-  res.type(handler.getContentType(fhirVersion));
-  res.status(200).json(json);
-}
-
 /**
  * @description Controller for all POST operations
  */
@@ -43,7 +14,7 @@ module.exports.operationsPost = function operationsPost({
     let { base_version, id } = req.sanitized_args;
     req.sanitized_args.resource = req.body;
     service[name](req.sanitized_args, { req }, deprecatedLogger)
-      .then((results) => handler.read(req, res, results))
+      .then((results) => handler.operation(req, res, results))
       .catch(next);
   };
 };
@@ -55,7 +26,7 @@ module.exports.operationsGet = function operationsGet({ profile, name, logger: d
   let { serviceModule: service } = profile;
   return (req, res, next) => {
     service[name](req.sanitized_args, { req }, deprecatedLogger)
-      .then((results) => read(req, res, results)) //handler.read(req, res, results))
+      .then((results) => handler.operation(req, res, results))
       .catch(next);
   };
 };
--- packages/node-fhir-server-core/src/server/service.mock.js
+++ packages/node-fhir-server-core/src/server/service.mock.js
@@ -81,8 +81,9 @@ module.exports.testPost = async (_args, { req }) => {
   logger.info(message);
   return {};
 };
-module.exports.testOp = async (_args, _context) => {
+module.exports.testOp = async (_args, { req }) => {
   let message = 'Calling mock service. Testing testOp';
   logger.info(message);
-  throw new ServerError(message, errorDetails(message));
+  req.res.status(202);
+  return {};
 };
