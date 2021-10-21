8c1814608bb0574e7826576c548fd39acc1250ed
natjoe4 <njjones@mitre.org>
2021-10-21 12:57:15 -0400

    updated comments

--- packages/node-fhir-server-core/src/server/operations/operations.controller.test.js
+++ packages/node-fhir-server-core/src/server/operations/operations.controller.test.js
@@ -3,14 +3,14 @@ const testConfig = require('../../test.config');
 const request = require('supertest');
 
 describe('Testing custom operations controller', () => {
-  test('test $testOp response code', async () => {
+  test('test $testOp response code is 202', async () => {
     let config = Object.assign({}, testConfig, { logging: { level: 'emerg' } });
     const server = new Server(config).setProfileRoutes().setErrorRoutes();
     let response = await request(server.app).post('/4_0_1/Patient/$testOp');
     expect(response.status).toEqual(202);
   });
 
-  test('test $testPost response code', async () => {
+  test('test $testPost response code is 200', async () => {
     let config = Object.assign({}, testConfig, { logging: { level: 'emerg' } });
     const server = new Server(config).setProfileRoutes().setErrorRoutes();
     let response = await request(server.app).post('/4_0_1/Patient/$testPost');
@@ -23,6 +23,7 @@ describe('Testing custom operations controller', () => {
     const testMock = jest.spyOn(mockFuncs, 'testPost');
 
     let config = Object.assign({}, testConfig, { logging: { level: 'emerg' } });
+    // Here we need to configure middleware since we want to pass in a request body
     const server = new Server(config).configureMiddleware().setProfileRoutes().setErrorRoutes();
     await request(server.app)
       .post('/4_0_1/Patient/$testPost')
