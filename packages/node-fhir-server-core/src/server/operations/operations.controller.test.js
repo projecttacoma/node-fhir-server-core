bf9732aceb6ce36f64c7ef5080a89c95701d9c79
natjoe4 <njjones@mitre.org>
2021-10-21 12:50:21 -0400

    added testing

--- packages/node-fhir-server-core/src/server/operations/operations.controller.test.js
+++ packages/node-fhir-server-core/src/server/operations/operations.controller.test.js
@@ -2,15 +2,48 @@ const Server = require('../server');
 const testConfig = require('../../test.config');
 const request = require('supertest');
 
-describe.skip('Testing custom operations controller', () => {
-  test('test', async () => {
+describe('Testing custom operations controller', () => {
+  test('test $testOp response code', async () => {
     let config = Object.assign({}, testConfig, { logging: { level: 'emerg' } });
     const server = new Server(config).setProfileRoutes().setErrorRoutes();
+    let response = await request(server.app).post('/4_0_1/Patient/$testOp');
+    expect(response.status).toEqual(202);
+  });
 
-    //let keys = Object.keys(server.config.profiles);
-    //let { routes } = require('../route.config');
-
-    let response = await request(server.app).get('/4_0_1/Patient/$testOp');
+  test('test $testPost response code', async () => {
+    let config = Object.assign({}, testConfig, { logging: { level: 'emerg' } });
+    const server = new Server(config).setProfileRoutes().setErrorRoutes();
+    let response = await request(server.app).post('/4_0_1/Patient/$testPost');
     expect(response.status).toEqual(200);
+  });
+
+  test('mock function is called with proper args', async () => {
+    // Use this spying format for all arg checking
+    const mockFuncs = require('../service.mock');
+    const testMock = jest.spyOn(mockFuncs, 'testPost');
+
+    let config = Object.assign({}, testConfig, { logging: { level: 'emerg' } });
+    const server = new Server(config).configureMiddleware().setProfileRoutes().setErrorRoutes();
+    await request(server.app)
+      .post('/4_0_1/Patient/$testPost')
+      .set('Accept', 'application/json+fhir')
+      .set('Content-Type', 'application/json+fhir')
+      .send({
+        id: '12345',
+        resourceType: 'Patient',
+      })
+      .expect(200);
+
+    expect(testMock).toHaveBeenCalledWith(
+      {
+        base_version: '4_0_1',
+        resource: {
+          resourceType: 'Patient',
+          id: '12345',
+        },
+      },
+      expect.anything(),
+      expect.anything()
+    );
   });
 });
